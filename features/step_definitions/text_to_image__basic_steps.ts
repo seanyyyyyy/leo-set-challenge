5f0ad66e18cdc57c78dc2b1e6210fcd95ffcae4e
sean <szliaw@gmail.com>
2024-06-15 00:29:12 +1000

    initial commit

---
+++ features/step_definitions/text_to_image__basic_steps.ts
@@ -0,0 +1,78 @@
+import {Browser, chromium, expect, Page} from "@playwright/test";
+import {Given, When, Then, setDefaultTimeout, AfterAll} from '@cucumber/cucumber';
+
+let browser: Browser;
+let page: Page;
+let prompt: string;
+
+setDefaultTimeout(60 * 1000);
+
+Given('a logged in user', async function () {
+    const username: string = 'szliaw@gmail.com'
+    const password: string = 'Password123!'
+
+    browser = await chromium.launch({headless: false})
+    page = await browser.newPage()
+    await page.goto('https://app.leonardo.ai/auth/login')
+
+    await page.getByRole('textbox', {name: 'email'}).fill(username)
+    await page.getByRole('textbox', {name: 'Password'}).fill(password)
+    await page.getByRole('button', {name: 'Sign in'}).click();
+
+    await expect(page).toHaveTitle('Home - Leonardo.Ai');
+    await expect(page).toHaveURL('https://app.leonardo.ai/');
+});
+
+When('the Image Generation page', async function () {
+    // close Getting Started modal #1
+    await page.getByRole('button', {name: 'Maybe Later'}).click()
+
+    await page.getByText('Image Generation').first().click()
+    await expect(page).toHaveTitle('Image Generation - Leonardo.Ai');
+    await expect(page).toHaveURL('https://app.leonardo.ai/image-generation');
+
+    // close Getting Started modal #2
+    await page.getByLabel('Close').click()
+    // close Intro Guide modal
+    await page.getByLabel('Close').click()
+});
+
+When('the {string} preset', async function (preset) {
+    await page.locator('.chakra-button.css-f2ftxr').click();
+    await page.getByRole('button', {name:preset}).click()
+    await page.getByRole('button', {name: 'Close panel'}).click()
+});
+
+When('Fast mode turned on', async function () {
+    await page.getByRole('button', {name: 'Fast'}).click()
+});
+
+When('a prompt of {string}', async function (featurePrompt) {
+    prompt = featurePrompt;
+    await page.getByPlaceholder('Type a prompt ...').fill(prompt)
+});
+
+When('image dimensions of {string}', async function (size) {
+    await page.getByRole('button', {name: size}).click()
+});
+
+When('aspect ratio is 1:1', async function () {
+    await page.getByRole('button', {name: '1:1'}).click()
+});
+
+When('Number of images is 4', async function () {
+    await page.getByRole('button', {name: '4', exact: true}).click()
+});
+
+When('the generate button is clicked', async function () {
+    await page.locator('.chakra-button.css-q7xawa').click();
+});
+
+Then('the generated image displays successfully', async function () {
+    const generatedImages = page.getByAltText(prompt)
+    await expect(generatedImages).toHaveCount(4, {timeout: 20000})
+});
+
+AfterAll(async function () {
+    await browser.close()
+});
---
+++ features/text_to_image_basic.feature
@@ -0,0 +1,14 @@
+Feature: Text-to-image
+  Basic text-to-image flow
+
+  Scenario: Basic text-to-image flow
+    Given a logged in user
+    And the Image Generation page
+    And the "Leonardo Lightning" preset
+    And Fast mode turned on
+    And a prompt of "test 18"
+    And image dimensions of "Small"
+    And aspect ratio is 1:1
+    And Number of images is 4
+    When the generate button is clicked
+    Then the generated image displays successfully
